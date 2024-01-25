b3199590be631afe93eadd3a86d6429b7ec14d8c
Joseph Nma <102808507+Josephnma@users.noreply.github.com>
2024-01-25 21:30:33 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import UserComponent from "./components/UserComponent";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <UserComponent />
+    </div>
+  );
+}
